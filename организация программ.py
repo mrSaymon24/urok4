036de7d9d00ee0d1e46acaca51c6fefe5e5ea055
mrSaymon24 <pisyaka95@gmail.com>
2024-09-13 00:02:41 +0700

    Add files via upload

---
+++ организация программ.py
@@ -0,0 +1,12 @@
+my_string= (input('поздаровайся: '))
+l=len(my_string)
+print (l)
+
+#работа с методом строк
+print ('my_string' .upper())
+print ('my_string' .lower())
+print ('my_string' .replace(('_'), ('')))
+print ('my_string' .__str__()[1:])
+print ('my_string' .__str__()[:-1])
+
+
