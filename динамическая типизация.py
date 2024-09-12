b9aa6e359d4e81876b3bf50c4be8669864d9fe80
mrSaymon24 <pisyaka95@gmail.com>
2024-09-12 20:02:15 +0700

    Add files via upload

---
+++ динамическая типизация.py
@@ -0,0 +1,8 @@
+name= 'Saymon'
+print ('имя:', (name))
+age=(31)
+print ('возраст:', (age))
+new_age= (age+6)
+print ('другой возраст:', (new_age))
+is_student= (31<37)
+print ('студент:', (is_student))
