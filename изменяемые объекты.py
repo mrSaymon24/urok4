d9759c22a3086e724cd347647f76e0ec1d387e0c
mrSaymon24 <pisyaka95@gmail.com>
2024-09-17 20:12:32 +0700

    Add files via upload

---
+++ изменяемые объекты.py
@@ -0,0 +1,4 @@
+school= ["pan", "note", "pancil", "cheir"]
+print (school)
+school [1]= "hand"
+print(school)
---
+++ неизменяемые и изменяемые объекты.py
@@ -0,0 +1,6 @@
+immutable_var= (1, 2, 3, 4, True, "string")
+print (immutable_var)
+immutable_var [3]= 777
+#изменить нельзя по умолчнии н програмном уровне, можно лишь чтото вынести из картежа
+
+
