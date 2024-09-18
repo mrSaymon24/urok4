dfb2a8d00111ed088cacd5a589c15e1ea509bd01
mrSaymon24 <pisyaka95@gmail.com>
2024-09-18 20:55:08 +0700

    Add files via upload

---
+++ множества.py
@@ -0,0 +1,9 @@
+my_set= {1, 2, 3, 4, 5, 1, 3, 2, 'String', (1, 2, 3)}
+print(my_set)
+my_set.add(45)
+print(my_set)
+my_set.add(11)
+print(my_set)
+my_set.remove(3)
+my_set.remove(5)
+print(my_set)
---
+++ словарь.py
@@ -0,0 +1,12 @@
+my_dict={'Andrey': 1994, 'Fedya': 1995, 'Petya': 1993}
+print(my_dict)
+print(my_dict['Fedya'])
+my_dict['Anton']=1997
+print(my_dict['Anton'])
+my_dict.update({'Kostya': 1998, 'Rita': 1999})
+print(my_dict)
+my_dict.pop('Petya')
+print(my_dict)
+
+
+
